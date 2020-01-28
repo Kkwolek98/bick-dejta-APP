142ae7673d3a0aeed09dbd606145746b7f8759a9
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-01-28 21:41:10 +0100

    website title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bick-dejta-app';
+  title = 'Bick Dejta - internetowy sklep z dużą ilością dejta';
 }
