7c99d1b02674671942ae8a40847b54111a471827
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-02-09 22:28:54 +0100

    generated components to be made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductListComponent } from './components/elements/product-list/product-list.component';
+import { ProductComponent } from './components/elements/product/product.component';
+import { CartViewComponent } from './components/cart-view/cart-view.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    ProductListComponent,
+    ProductComponent,
+    CartViewComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
