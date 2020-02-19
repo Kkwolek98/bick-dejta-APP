b19bb2deb1bc6393231638f7f9752f8f97f49445
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-02-19 21:09:05 +0100

    fix for fix that fixes fix

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -1,12 +1,13 @@
 import { Platform } from './Platform';
+import { Category } from './Category';
 
 export class Product {
     constructor(
         public name: string,
         public description: string,
-        public imgUrl: string,
-        public platforms: Platform[],
-        public categories: Category[],
+        public imgUrl?: string,
+        public platforms?: Platform[],
+        public categories?: Category[],
         public id?: number,
     ) { }
 
