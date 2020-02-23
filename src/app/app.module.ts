479d852393040c449ab66dba1e25dac36b6482ad
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-02-23 21:15:19 +0100

    uncommented chip autocomplete module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,8 @@ import { AdminPanelComponent } from './components/admin-panel/admin-panel.compon
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
-// import { ChipAutocompleteComponent } from './components/elements/chip-autocomplete/chip-autocomplete.component';
-import {MatListModule} from '@angular/material/list'; 
+import { ChipAutocompleteComponent } from './components/elements/chip-autocomplete/chip-autocomplete.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -31,7 +31,7 @@ import {MatListModule} from '@angular/material/list';
     AboutComponent,
     AdminPanelComponent,
     ProductDetailsComponent,
-    // ChipAutocompleteComponent
+    ChipAutocompleteComponent
   ],
   imports: [
     BrowserModule,
