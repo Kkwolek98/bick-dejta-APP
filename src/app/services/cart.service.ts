11d8b0b7a76d421c8e6e7ef4314f4fe5b6f7f0fa
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-02-25 22:37:59 +0100

    cart -> if item is in cart, add ammount

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,22 +7,36 @@ export class CartService {
   private key_localStorage: string = 'games';
   constructor() {
 
-   }
+  }
 
-  addToCart(product_id: number, platform_id: number){
-    if(localStorage.getItem(this.key_localStorage) === null){
+  addToCart(product_id: number, platform_id: number) {
+    if (localStorage.getItem(this.key_localStorage) === null) {
       let products = [];
-      products.push({'product_id': product_id,'platform_id': platform_id});
-      localStorage.setItem(this.key_localStorage,JSON.stringify(products));
-    }else{
+      let prod = products.find(x => x.platform_id === platform_id);
+      let prodIndex = products.findIndex(x => x.platform_id === platform_id);
+      if (prod) {
+        products[prodIndex]['ammount']++;
+      } else {
+        products.push({ 'platform_id': platform_id, 'ammount': 1 });
+      }
+      localStorage.setItem(this.key_localStorage, JSON.stringify(products));
+    } else {
       let product = localStorage.getItem(this.key_localStorage);
       let products = JSON.parse(product);
-      products.push({'product_id': product_id,'platform_id': platform_id});
-      localStorage.setItem(this.key_localStorage,JSON.stringify(products)); 
+      let prod = products.find(x => x.platform_id === platform_id);
+      let prodIndex = products.findIndex(x => x.platform_id === platform_id);
+      if (prod) {
+        products[prodIndex]['ammount']++;
+      } else {
+        products.push({ 'platform_id': platform_id, 'ammount': 1 });
+      }
+
+      localStorage.setItem(this.key_localStorage, JSON.stringify(products));
+
     }
   }
 
-  getProducts():any[]{
+  getProducts(): any[] {
     return JSON.parse(localStorage.getItem(this.key_localStorage));
   }
 
