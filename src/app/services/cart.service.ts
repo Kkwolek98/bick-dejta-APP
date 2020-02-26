de916e20196fcdd8bf94a1356dbba50077efdcc9
Katarzyna Knapik <katarzyna_knapik@onet.pl>
2020-02-26 21:32:36 +0100

    cart done

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,31 +10,46 @@ export class CartService {
   }
 
   addToCart(product_id: number, platform_id: number) {
+    let products = [];
+    
     if (localStorage.getItem(this.key_localStorage) === null) {
-      let products = [];
       let prod = products.find(x => x.platform_id === platform_id);
       let prodIndex = products.findIndex(x => x.platform_id === platform_id);
       if (prod) {
         products[prodIndex]['ammount']++;
       } else {
-        products.push({ 'platform_id': platform_id, 'ammount': 1 });
+        products.push({'product_id': product_id, 'platform_id': platform_id, 'ammount': 1 });
       }
-      localStorage.setItem(this.key_localStorage, JSON.stringify(products));
     } else {
-      let product = localStorage.getItem(this.key_localStorage);
-      let products = JSON.parse(product);
+      products = JSON.parse(localStorage.getItem(this.key_localStorage));
       let prod = products.find(x => x.platform_id === platform_id);
       let prodIndex = products.findIndex(x => x.platform_id === platform_id);
       if (prod) {
         products[prodIndex]['ammount']++;
       } else {
-        products.push({ 'platform_id': platform_id, 'ammount': 1 });
+        products.push({ 'product_id': product_id, 'platform_id': platform_id, 'ammount': 1 });
       }
-
-      localStorage.setItem(this.key_localStorage, JSON.stringify(products));
-
     }
+
+    localStorage.setItem(this.key_localStorage, JSON.stringify(products));
   }
+
+  removeFromCart(platform_id: number){
+    let product = localStorage.getItem(this.key_localStorage);
+    let products = JSON.parse(product);
+    let prod = products.find(x => x.platform_id === platform_id);
+    let prodIndex = products.findIndex(x => x.platform_id === platform_id);
+    if (prod) {
+      products[prodIndex]['ammount']--;
+      if(products[prodIndex]['ammount'] === 0){
+        delete products[prodIndex];
+        products = products.filter(el => el != null)
+      }
+    }
+
+    localStorage.setItem(this.key_localStorage, JSON.stringify(products));
+  }
+
 
   getProducts(): any[] {
     return JSON.parse(localStorage.getItem(this.key_localStorage));
