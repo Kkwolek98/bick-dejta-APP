2b02f867d4d60595efa55e2d4c201887250c2e94
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-02-26 23:39:42 +0100

    fix fix to fix fix

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,14 +15,14 @@ export class CartService {
 
   addToCart(product_id: number, platform_id: number) {
     let products = [];
-    
+
     if (localStorage.getItem(this.key_localStorage) === null) {
       let prod = products.find(x => x.platform_id === platform_id);
       let prodIndex = products.findIndex(x => x.platform_id === platform_id);
       if (prod) {
         products[prodIndex]['amount']++;
       } else {
-        products.push({'productId': product_id, 'platformId': platform_id, 'amount': 1 });
+        products.push({ 'platformId': platform_id, 'amount': 1 });
       }
     } else {
       products = JSON.parse(localStorage.getItem(this.key_localStorage));
@@ -31,21 +31,21 @@ export class CartService {
       if (prod) {
         products[prodIndex]['amount']++;
       } else {
-        products.push({ 'productId': product_id, 'platformId': platform_id, 'amount': 1 });
+        products.push({ 'platformId': platform_id, 'amount': 1 });
       }
     }
 
     localStorage.setItem(this.key_localStorage, JSON.stringify(products));
   }
 
-  removeFromCart(platform_id: number){
+  removeFromCart(platform_id: number) {
     let product = localStorage.getItem(this.key_localStorage);
     let products = JSON.parse(product);
     let prod = products.find(x => x.platform_id === platform_id);
     let prodIndex = products.findIndex(x => x.platform_id === platform_id);
     if (prod) {
       products[prodIndex]['ammount']--;
-      if(products[prodIndex]['ammount'] === 0){
+      if (products[prodIndex]['ammount'] === 0) {
         delete products[prodIndex];
         products = products.filter(el => el != null)
       }
@@ -57,9 +57,9 @@ export class CartService {
   getPriceTotal() {
     let products = JSON.parse(localStorage.getItem(this.key_localStorage));
     products.forEach(e => {
-        delete e.productId
+      delete e.productId
     });
-    
+
     this.http.post(urls.API + "/price-total", {
       items: JSON.stringify(products)
     })
